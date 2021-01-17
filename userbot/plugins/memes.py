09d7257a993bacc8b4fb35b8add61e63afa5d7d3
LEGEND X <74249238+legendx22@users.noreply.github.com>
2021-01-17 20:54:25 +0530

    Hmm

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -2,7 +2,7 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.b (the "License");
 # you may not use this file except in compliance with the License.
-#
+##op
 #
 
 """ Userbot module for having some fun with people. """
